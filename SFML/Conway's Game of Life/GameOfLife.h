1a5c5b270bb0e535f2d2ff6250b34ebd88a4b373
Aschii6 <120491549+Aschii6@users.noreply.github.com>
2023-07-13 21:34:20 +0300

    Update GameOfLife.h

--- SFML/Conway's Game of Life/GameOfLife.h
+++ SFML/Conway's Game of Life/GameOfLife.h
@@ -1,69 +1,59 @@
-#pragma once
-#include <vector>
+#pragma once#include <vector>
 
 using namespace std;
 
-class GameOfLife
-{
-private:
-	vector<vector<bool>> grid;
+class GameOfLife {
+  private: vector < vector < bool >> grid;
 
-public:
-	GameOfLife(int x, int y) {
-		grid.resize(x);
-		for (int i = 0; i < x; i++)
-		{
-			grid[i].resize(y);
-			for (int j = 0; j < y; j++)
-			{
-				grid[i][j] = false;
-			}
-		}
-	}
+  public: GameOfLife(int x, int y) {
+      grid.resize(x);
+      for (int i = 0; i < x; i++) {
+        grid[i].resize(y);
+        for (int j = 0; j < y; j++) {
+          grid[i][j] = false;
+        }
+      }
+    }
 
-	~GameOfLife() = default;
+    ~GameOfLife() = default;
 
-	vector<vector<bool>> getGrid() {
-		return grid;
-	}
+  vector < vector < bool >> getGrid() {
+    return grid;
+  }
 
-	void evolve() {
-		vector<vector<bool>> newGrid = grid;
+  void evolve() {
+    vector < vector < bool >> newGrid = grid;
 
-		for (int i = 0; i < grid.size(); i++)
-		{
-			for (int j = 0; j < grid[i].size(); j++)
-			{
-				int neighbors = 0;
+    for (int i = 0; i < grid.size(); i++) {
+      for (int j = 0; j < grid[i].size(); j++) {
+        int neighbors = 0;
 
-				if (i > 0 && j > 0 && grid[i - 1][j - 1]) neighbors++;
-				if (i > 0 && grid[i - 1][j]) neighbors++;
-				if (i > 0 && j < grid[i].size() - 1 && grid[i - 1][j + 1]) neighbors++;
-				if (j > 0 && grid[i][j - 1]) neighbors++;
-				if (j < grid[i].size() - 1 && grid[i][j + 1]) neighbors++;
-				if (i < grid.size() - 1 && j > 0 && grid[i + 1][j - 1]) neighbors++;
-				if (i < grid.size() - 1 && grid[i + 1][j]) neighbors++;
-				if (i < grid.size() - 1 && j < grid[i].size() - 1 && grid[i + 1][j + 1]) neighbors++;
+        if (i > 0 && j > 0 && grid[i - 1][j - 1]) neighbors++;
+        if (i > 0 && grid[i - 1][j]) neighbors++;
+        if (i > 0 && j < grid[i].size() - 1 && grid[i - 1][j + 1]) neighbors++;
+        if (j > 0 && grid[i][j - 1]) neighbors++;
+        if (j < grid[i].size() - 1 && grid[i][j + 1]) neighbors++;
+        if (i < grid.size() - 1 && j > 0 && grid[i + 1][j - 1]) neighbors++;
+        if (i < grid.size() - 1 && grid[i + 1][j]) neighbors++;
+        if (i < grid.size() - 1 && j < grid[i].size() - 1 && grid[i + 1][j + 1]) neighbors++;
 
-				if (grid[i][j]) {
-					if (neighbors < 2 || neighbors > 3) {
-						newGrid[i][j] = false;
-					}
-				}
-				else {
-					if (neighbors == 3) {
-						newGrid[i][j] = true;
-					}
-				}
-			}
-		}
-		grid = newGrid;
-	}
+        if (grid[i][j]) {
+          if (neighbors < 2 || neighbors > 3) {
+            newGrid[i][j] = false;
+          }
+        } else {
+          if (neighbors == 3) {
+            newGrid[i][j] = true;
+          }
+        }
+      }
+    }
+    grid = newGrid;
+  }
 
-	void setCell(int x, int y, bool newVal) {
-		if (x >= 0 && x < grid.size() && y >= 0 && y < grid[x].size()) {
-			grid[x][y] = newVal;
-		}
-	}
+  void setCell(int x, int y, bool newVal) {
+    if (x >= 0 && x < grid.size() && y >= 0 && y < grid[x].size()) {
+      grid[x][y] = newVal;
+    }
+  }
 };
-
